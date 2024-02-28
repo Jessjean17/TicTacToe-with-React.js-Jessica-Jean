98a7db5d42eca49dc08a1d8f960b5a9cbb7ef15f
Jessica Jean <140281920+Jessjean17@users.noreply.github.com>
2024-02-28 15:06:15 +0000

    add

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,94 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { Board } from "../component/board.js";
+import { ChoosePlayer } from "../component/chooseplayer.js";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+export class Home extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			player: null,
+			winner: null
+		};
+	}
 
+	startGame = () => {
+		console.log("Start Game");
+		this.setState({ player: null });
+	};
+
+	setTurn = (currentPlayer, player1, player2) => {
+		this.setState({
+			player: currentPlayer,
+			player1: player1,
+			player2: player2
+		});
+	};
+
+	nextMove = position => {
+		console.log("Next move at position: ", position);
+		this.setState({
+			player: this.state.player == "x" ? "o" : "x"
+		});
+	};
+
+	declareWinner = winner => {
+		this.setState({
+			winner: winner
+		});
+	};
+
+	renderMessage = () => {
+		if (this.state.player == null) {
+			return <h2 id="message">Pick A Weapon</h2>;
+		} else {
+			if (this.state.winner == null) {
+				return (
+					<div>
+						<h2>It is {this.state.player.toUpperCase()} turn!</h2>
+						<button
+							className="btn btn-lg btn-secondary"
+							onClick={this.startOver}>
+							Start Over
+						</button>
+					</div>
+				);
+			} else {
+				return (
+					<div>
+						<h1 style={{ color: "green" }}>
+							{this.state.winner.toUpperCase()} WINS!!!
+						</h1>
+						<button
+							className="btn btn-lg btn-success"
+							style={{ background: "green" }}
+							onClick={this.startOver}>
+							Start Over!
+						</button>
+					</div>
+				);
+			}
+		}
+	};
+
+	render() {
+		return (
+			<div id="msg">
+				<h1>
+					Tic Tac Toe <small>in React.js</small>
+				</h1>
+				{this.renderMessage()}
+				<Board
+					currentPlayer={this.state.player}
+					onMove={this.nextMove}
+					onWinner={this.declareWinner}
+				/>
+				<ChoosePlayer
+					hide={this.state.player == null ? true : false}
+					onSetTurn={this.setTurn}
+				/>
+			</div>
+		);
+	}
+}
 export default Home;
